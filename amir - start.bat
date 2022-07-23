143fcdbb5321da76c4420af7d50ad9c5d47fa7d4
amir <75942838+72c@users.noreply.github.com>
2022-07-23 08:32:00 +0100

    added new start.bats for our diff directories

---
+++ amir - start.bat
@@ -0,0 +1,2 @@
+cd /d D:\Localhosts\amir-fw\cfx-server-data
+D:\Localhosts\amir-fw\[artifact]\FXServer.exe +exec server.cfg +exec resources.cfg +set gamename gta +set sv_enforceGameBuild 1436 +set onesync on
